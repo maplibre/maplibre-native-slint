d0d3dd6db7c3fb661199c5031ae5a6cc971a9f19
yuiseki <yuiseki@gmail.com>
2025-08-23 09:35:11 +0900

    Support Linux desktop (#5)
    * feat: support Linux: Implement MapObserver interface and enhance map initialization process
    
    * docs: add more project documentation and GitHub templates
    
    * feat: support linux desktop

--- src/slint_maplibre.hpp
+++ src/slint_maplibre.hpp
@@ -1,29 +1,40 @@
 #pragma once
 
-#include <slint.h>
-#include <mbgl/map/map.hpp>
 #include <mbgl/gfx/headless_frontend.hpp>
-#include <mbgl/storage/resource_options.hpp>
-#include <mbgl/map/map_options.hpp>
+#include <mbgl/map/map.hpp>
 #include <mbgl/map/map_observer.hpp>
+#include <mbgl/map/map_options.hpp>
+#include <mbgl/storage/resource_options.hpp>
 #include <mbgl/util/run_loop.hpp>
+#include <slint.h>
 
 #include "../platform/custom_file_source.hpp"
 
+#include <atomic>
 #include <memory>
 #include <atomic>
 
 class SlintMapLibre : public mbgl::MapObserver {
 public:
-    SlintMapLibre();
-    ~SlintMapLibre();
+  SlintMapLibre();
+  ~SlintMapLibre();
 
-    void initialize(int width, int height);
-    slint::Image render_map();
-    void resize(int width, int height);
-    void handle_mouse_press(float x, float y);
-    void handle_mouse_release(float x, float y);
-    void handle_mouse_move(float x, float y, bool pressed);
+  void initialize(int width, int height);
+  slint::Image render_map();
+  void resize(int width, int height);
+  void handle_mouse_press(float x, float y);
+  void handle_mouse_release(float x, float y);
+  void handle_mouse_move(float x, float y, bool pressed);
+
+  // Manually drive the map's run loop
+  void run_map_loop();
+
+  // MapObserver implementation
+  void onWillStartLoadingMap() override;
+  void onDidFinishLoadingStyle() override;
+  void onDidBecomeIdle() override;
+  void onDidFailLoadingMap(mbgl::MapLoadError error,
+                           const std::string &what) override;
 
     // MapObserver implementation
     void onWillStartLoadingMap() override;
@@ -32,17 +43,17 @@ public:
     void onDidFailLoadingMap(mbgl::MapLoadError error, const std::string& what) override;
 
 private:
-    std::unique_ptr<mbgl::Map> map;
-    std::unique_ptr<mbgl::HeadlessFrontend> frontend;
-    std::unique_ptr<mbgl::CustomFileSource> file_source;
-    std::unique_ptr<mbgl::util::RunLoop> run_loop;
+  std::unique_ptr<mbgl::Map> map;
+  std::unique_ptr<mbgl::HeadlessFrontend> frontend;
+  std::unique_ptr<mbgl::CustomFileSource> file_source;
+  std::unique_ptr<mbgl::util::RunLoop> run_loop;
 
-    int width = 0;
-    int height = 0;
+  int width = 0;
+  int height = 0;
 
-    mbgl::Point<double> last_pos;
-    
-    // スタイル読み込み状態管理
-    std::atomic<bool> style_loaded{false};
-    std::atomic<bool> map_idle{false};
+  mbgl::Point<double> last_pos;
+
+  // スタイル読み込み状態管理
+  std::atomic<bool> style_loaded{false};
+  std::atomic<bool> map_idle{false};
 };

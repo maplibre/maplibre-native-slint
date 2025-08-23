0aeb9672298a8aacbdcb6e9787495d456d363534
yuiseki <yuiseki@gmail.com>
2025-08-23 14:13:57 +0900

    feat(rendering): Implement asynchronous rendering callback for MapLibre (#23)
    * feat(rendering): Implement asynchronous rendering callback for MapLibre
    
    * style: Format comment for clarity in main.cpp
    
    * feat(docs): Add clang-format to required system packages for macOS build

--- src/slint_maplibre.hpp
+++ src/slint_maplibre.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <atomic>
+#include <functional>
 #include <mbgl/gfx/headless_frontend.hpp>
 #include <mbgl/map/map.hpp>
 #include <mbgl/map/map_observer.hpp>
@@ -18,6 +19,7 @@ public:
     ~SlintMapLibre();
 
     void initialize(int width, int height);
+    void setRenderCallback(std::function<void()> callback);
     slint::Image render_map();
     void resize(int width, int height);
     void handle_mouse_press(float x, float y);
@@ -39,6 +41,7 @@ private:
     std::unique_ptr<mbgl::HeadlessFrontend> frontend;
     std::unique_ptr<mbgl::CustomFileSource> file_source;
     std::unique_ptr<mbgl::util::RunLoop> run_loop;
+    std::function<void()> m_renderCallback;
 
     int width = 0;
     int height = 0;
